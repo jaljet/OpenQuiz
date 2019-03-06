9e2db37ef95477d4a7f1f66d9767378d6c405a75
alexsosta <44973269+alexsosta@users.noreply.github.com>
2019-03-06 11:58:12 +0300

    11:57-06.03.2019

--- misc/setupSql.sql
+++ misc/setupSql.sql
@@ -3,4 +3,4 @@ insert into roles (id, role) values (400001, 'admins');
 insert into roles (id, role) values (400002, 'players');
 insert into users (id, email, password, username, office, rusname) values (300991, 'admin@email.com', 'OgJj5Z45bLeKEP19pHiXSdCFjtyDUhq3XzWaQct5KIbxBBiJ2EvJ', 'admin','','');
 insert into users_roles (user_id, role_id) values (300991, 400001);
-insert into rule (id, rule ) values (400003, 'Викторина состоит из 10 вопросов')
+insert into rule (id, rule ) values (400003, 'Quiz consists of 10 questions')
